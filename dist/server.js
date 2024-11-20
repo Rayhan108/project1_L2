0dfbc10deba80e681b99d7cf87ea6ae9da27916e
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-20 23:48:26 +0600

    succrsfully crated data and retrived this data

--- dist/server.js
+++ dist/server.js
@@ -12,16 +12,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-// import app from "./app";
+const app_1 = __importDefault(require("./app"));
 const confiq_1 = __importDefault(require("./app/confiq"));
 const mongoose_1 = __importDefault(require("mongoose"));
-const app = (0, express_1.default)();
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield mongoose_1.default.connect(confiq_1.default.database_url);
-            app.listen(confiq_1.default.port, () => {
+            app_1.default.listen(confiq_1.default.port, () => {
                 console.log(`Example app listening on port ${confiq_1.default.port}`);
             });
         }

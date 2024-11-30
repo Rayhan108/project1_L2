c5e0bcb52937e0ecd45590a1110dfe6f12fa3066
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-30 17:50:33 +0600

    refactor routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
-import { StudentRoutes } from './app/modules/student/student.route'
-import { UserRoutes } from './app/modules/user/user.routes'
+
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import notFound from './app/middlewares/notFound'
+import router from './app/routes'
 
 const app :Application = express()
 
@@ -13,8 +13,8 @@ const app :Application = express()
 app.use(express.json())
 app.use(cors())
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+
+app.use('/api/v1', router);
 
 const getAController = (req: Request, res: Response) => {
   const a = 10;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { UserRoutes } from '../modules/user/user.routes';
+import { StudentRoutes } from '../modules/student/student.route';
+
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;

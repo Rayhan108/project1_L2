33a00e58092eff58b6dd21a9715273cf45c6ee00
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-12-19 18:50:17 +0600

    verify-token-using-auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,15 +1,26 @@
-
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 import catchAsync from "../utils/catchAsync";
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
+import confiq from '../confiq';
+
+
 const auth = ()=>{
     return catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
   const token = req.headers.authorization;
+  //check if token sent from the client
   if(!token){
     throw new AppError(httpStatus.UNAUTHORIZED,"you are not authorized")
   }
-  next();
+  //check if the token is valid
+  jwt.verify(token,confiq.jwt_access_secret as string,function(err,decoded){
+    if(err){
+        throw new AppError(httpStatus.UNAUTHORIZED,"you are not authorized")
+      }
+      req.user=decoded as JwtPayload;
+      next();
+  })
        })
 }
 export default auth
